df971981a23f725dec7d1079fdc53c88a6a6886e
unknown <kevin018111@gmail.com>
2024-04-02 23:16:55 +0900

    9번하는중

---
+++ 윈도우 프로그래밍/visualstudio/20240402_s_2_9/20240402_s_2_9/main.cpp
@@ -0,0 +1,141 @@
+#include <Windows.h>
+#include <tchar.h>
+#include <random>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+HINSTANCE g_hInst;
+LPCTSTR lpszClass = L"실습 2-9번";
+LPCTSTR lpszWindowName = L"실습 2-9번";
+
+#define WIDTH 1000
+#define HEIGHT 800
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow) {
+
+    HWND hWnd;
+    MSG Message;
+    WNDCLASSEX WndClass;
+    g_hInst = hInstance;
+
+    WndClass.cbSize = sizeof(WndClass);
+    WndClass.style = CS_HREDRAW | CS_VREDRAW;
+    WndClass.lpfnWndProc = (WNDPROC)WndProc;
+    WndClass.cbClsExtra = 0;
+    WndClass.cbWndExtra = 0;
+    WndClass.hInstance = hInstance;
+    WndClass.hIcon = LoadIcon(NULL, IDI_QUESTION);
+    WndClass.hCursor = LoadCursor(NULL, IDC_HAND);
+    WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+    WndClass.lpszMenuName = NULL;
+    WndClass.lpszClassName = lpszClass;
+    WndClass.hIconSm = LoadIcon(NULL, IDI_QUESTION);
+    RegisterClassEx(&WndClass);
+
+    hWnd = CreateWindow(lpszClass, lpszWindowName, WS_OVERLAPPEDWINDOW, 0, 0, WIDTH, HEIGHT, NULL, (HMENU)NULL, hInstance, NULL);
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    while (GetMessage(&Message, 0, 0, 0)) {
+        TranslateMessage(&Message);
+        DispatchMessage(&Message);
+    }
+
+    return Message.wParam;
+
+}
+
+class Shape {
+public:
+    virtual void draw(HDC hdc, RECT rect) const = 0; // 순수 가상 함수
+    virtual ~Shape() {} // 가상 소멸자
+};
+
+// 사각형 클래스
+class RectangleShape : public Shape {
+    RECT rect;
+    COLORREF color;
+
+public:
+    RectangleShape(RECT rect, COLORREF color) : rect(rect), color(color) {}
+
+    void draw(HDC hdc,RECT rect) const override {
+        HBRUSH hBrush = CreateSolidBrush(color);
+        FillRect(hdc, &rect, hBrush);
+        DeleteObject(hBrush);
+    }
+};
+
+// 삼각형 클래스
+class RectangleShape : public Shape {
+    RECT rect;
+    COLORREF color;
+
+public:
+    RectangleShape(RECT rect, COLORREF color) : rect(rect), color(color) {}
+
+    void draw(HDC hdc, RECT rect) const override {
+        HBRUSH hBrush = CreateSolidBrush(color);
+        FillRect(hdc, &rect, hBrush);
+        DeleteObject(hBrush);
+    }
+};
+
+
+#define DEFAULT_COL 16
+
+random_device rd;
+mt19937 gen(rd());
+uniform_int_distribution<int> uidX(0, 3);
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    PAINTSTRUCT ps;
+    HDC hDC;
+    HPEN hPen, oldPen;
+    HBRUSH hBrush, oldBrush;
+    static RECT rect;
+    static SIZE size;
+ 
+    
+    switch (uMsg)
+    {
+    case WM_CREATE:
+        
+        break;
+    case WM_CHAR:
+        break;
+    case WM_PAINT:
+        hDC = BeginPaint(hWnd, &ps);
+
+        GetClientRect(hWnd, &rect);
+
+        hPen = CreatePen(PS_SOLID, 2, RGB(0, 0, 0));
+        oldPen = (HPEN)SelectObject(hDC, hPen);
+        hBrush = CreateSolidBrush(RGB(128, 128, 128));
+        oldBrush = (HBRUSH)SelectObject(hDC, hBrush);
+        Rectangle(hDC, (rect.right / 2) - 100, (rect.bottom / 2) - 100, (rect.right / 2) + 100, (rect.bottom / 2) + 100);
+
+        SelectObject(hDC, oldPen); // 제자리 돌아가기
+        SelectObject(hDC, oldBrush);
+        DeleteObject(hPen); // 새로운 객체 삭제
+        DeleteObject(hBrush);
+
+        EndPaint(hWnd, &ps);
+        ReleaseDC(hWnd, hDC);
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    case WM_KEYDOWN:
+        break;
+
+    default:
+        break;
+    }
+
+    return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
