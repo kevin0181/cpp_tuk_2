f6c4aa1b637834ea0a4e196856cf5449e1f9f257
youngbeen you <kevin018111@gmail.com>
2024-04-12 13:00:40 +0900

    123

---
+++ 윈도우 프로그래밍/visualstudio/20240409_s_3_1/20240409_s_3_1/main.cpp
@@ -0,0 +1,112 @@
+﻿#include <Windows.h>
+#include <tchar.h>
+#include <random>
+#include <vector>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+HINSTANCE g_hInst;
+LPCTSTR lpszClass = L"실습 3-1";
+LPCTSTR lpszWindowName = L"실습 3-1";
+
+#define WIDTH 1200
+#define HEIGHT 860
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow) {
+
+    HWND hWnd;
+    MSG Message;
+    WNDCLASSEX WndClass;
+    g_hInst = hInstance;
+
+    WndClass.cbSize = sizeof(WndClass);
+    WndClass.style = CS_HREDRAW | CS_VREDRAW;
+    WndClass.lpfnWndProc = (WNDPROC)WndProc;
+    WndClass.cbClsExtra = 0;
+    WndClass.cbWndExtra = 0;
+    WndClass.hInstance = hInstance;
+    WndClass.hIcon = LoadIcon(NULL, IDI_QUESTION);
+    WndClass.hCursor = LoadCursor(NULL, IDC_HAND);
+    WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+    WndClass.lpszMenuName = NULL;
+    WndClass.lpszClassName = lpszClass;
+    WndClass.hIconSm = LoadIcon(NULL, IDI_QUESTION);
+    RegisterClassEx(&WndClass);
+
+    hWnd = CreateWindow(lpszClass, lpszWindowName, WS_OVERLAPPEDWINDOW | WS_VSCROLL, 0, 0, WIDTH, HEIGHT, NULL, (HMENU)NULL, hInstance, NULL);
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    while (GetMessage(&Message, 0, 0, 0)) {
+        TranslateMessage(&Message);
+        DispatchMessage(&Message);
+    }
+
+    return Message.wParam;
+
+}
+
+#define cellSize 40
+#define gridSize 20
+
+random_device rd;
+mt19937 gen(rd());
+uniform_int_distribution<int> uid_RGB(0, 255);
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+
+    PAINTSTRUCT ps;
+    HDC hDC;
+    HPEN hPen, oldPen;
+    HBRUSH hBrush, oldBrush;
+    static RECT rect;
+    static SIZE size;
+    switch (uMsg)
+    {
+    case WM_CREATE:
+    {
+
+    }
+    break;
+    case WM_KEYUP:
+        break;
+    case WM_KEYDOWN:
+
+        hDC = BeginPaint(hWnd, &ps);
+
+        InvalidateRect(hWnd, NULL, TRUE);
+
+        EndPaint(hWnd, &ps);
+        break;
+    case WM_CHAR:
+
+        switch (wParam)
+        {
+        default:
+            break;
+        }
+        
+        InvalidateRect(hWnd, NULL, TRUE);
+
+        break;
+    case WM_SIZE:
+        break;
+    case WM_PAINT:
+    {
+        hDC = BeginPaint(hWnd, &ps);
+        EndPaint(hWnd, &ps);
+        break;
+    }
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        break;
+    }
+
+    return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
