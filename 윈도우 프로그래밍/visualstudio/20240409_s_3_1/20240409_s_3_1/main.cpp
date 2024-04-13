1c818ab74bbf840f72fc318959ec7b6b51f6c14e
unknown <kevin018111@gmail.com>
2024-04-13 12:46:45 +0900

    1

--- 윈도우 프로그래밍/visualstudio/20240409_s_3_1/20240409_s_3_1/main.cpp
+++ 윈도우 프로그래밍/visualstudio/20240409_s_3_1/20240409_s_3_1/main.cpp
@@ -50,6 +50,14 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdPa
 
 }
 
+
+#define gridSize 40
+
+random_device rd;
+mt19937 gen(rd());
+uniform_int_distribution<int> uid_RGB(0, 255);
+uniform_int_distribution<int> uid_ran_4(0, 3);
+uniform_int_distribution<int> uid_position(0, 39);
 struct shape {
     int positionX;
     int positionY;
@@ -58,8 +66,11 @@ struct shape {
     int cellSizeX;
     int cellSizeY;
     int move_i;
+    COLORREF colorCircle = RGB(255, 0, 0);
+    COLORREF colorFood = RGB(uid_RGB(gen), uid_RGB(gen), uid_RGB(gen));
+    bool m1_status = false;
 
-    void print_shape(HDC& mDC) {
+    void print_circle_shape(HDC& mDC) {
 
         circleRect.left = 0;
         circleRect.top = 0;
@@ -71,19 +82,32 @@ struct shape {
         circleRect.top += (positionY * cellSizeY);
         circleRect.bottom += (positionY * cellSizeY);
 
-        mBrush = CreateSolidBrush(RGB(255, 0, 0));
+        mBrush = CreateSolidBrush(colorCircle);
         oldBrush = (HBRUSH)SelectObject(mDC, mBrush);
         Ellipse(mDC, circleRect.left, circleRect.top, circleRect.right, circleRect.bottom);
         DeleteObject(mBrush);
     }
+
+    void print_food_shape(HDC& mDC) {
+        circleRect.left = 0;
+        circleRect.top = 0;
+        circleRect.right = cellSizeX;
+        circleRect.bottom = cellSizeY;
+
+        circleRect.left += (positionX * cellSizeX) + 2;
+        circleRect.top += (positionY * cellSizeY) + 2;
+
+        circleRect.right += (positionX * cellSizeX) - 2;
+        circleRect.bottom += (positionY * cellSizeY) - 2;
+
+        mBrush = CreateSolidBrush(colorFood);
+        oldBrush = (HBRUSH)SelectObject(mDC, mBrush);
+        Rectangle(mDC, circleRect.left, circleRect.top, circleRect.right, circleRect.bottom);
+        DeleteObject(mBrush);
+    }
+
 };
 
-#define gridSize 20
-
-random_device rd;
-mt19937 gen(rd());
-uniform_int_distribution<int> uid_RGB(0, 255);
-uniform_int_distribution<int> uid_ran_4(0, 3);
 LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 
     PAINTSTRUCT ps;
@@ -99,6 +123,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     static int hero_positionY{};
     static bool start_status = false;
     static int Timer1Count = 0;
+    static int Timer2Count = 0;
     static bool hero_lineX_status = false;
     static bool hero_lineY_status = false;
 
@@ -108,11 +133,13 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     {
     case WM_CREATE:
     {
-        SetTimer(hWnd, 1, 1, NULL);
-
+        SetTimer(hWnd, 1, 100, NULL);
+        SetTimer(hWnd, 2, 3000, NULL);
         for (int i = 0; i < 20; ++i) {
             shape shape1;
             shape1.move_i = uid_ran_4(gen);
+            shape1.positionX = uid_position(gen);
+            shape1.positionY = uid_position(gen);
             shapes.push_back(shape1);
         }
 
@@ -175,8 +202,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
                 hero_positionX += hero_lineX_status ? -1 : 1;
 
                 // 수평 이동에 대한 경계 검사
-                if (hero_positionX > 19) {
-                    hero_positionX = 19; // 최대값으로 제한
+                if (hero_positionX > 39) {
+                    hero_positionX = 39; // 최대값으로 제한
                     hero_lineX_status = true; // 방향을 왼쪽으로 변경
                     hero_positionY += hero_lineY_status ? -1 : 1; // 수직 이동
                 }
@@ -187,8 +214,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
                 }
 
                 // 수직 경계 검사 및 방향 변경
-                if (hero_positionY > 19) {
-                    hero_positionY = 19; // 아래쪽으로 제한
+                if (hero_positionY > 39) {
+                    hero_positionY = 39; // 아래쪽으로 제한
                     hero_lineY_status = true; // 방향을 위로 변경
                 }
                 else if (hero_positionY < 0) {
@@ -198,14 +225,45 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
             }
             shapes[0].positionX = hero_positionX;
             shapes[0].positionY = hero_positionY;
-            shapes[0].print_shape(mDC);
+            shapes[0].print_circle_shape(mDC);
         }
         else {
             shapes[0].positionX = 0;
             shapes[0].positionY = 0;
-            shapes[0].print_shape(mDC);
+            shapes[0].print_circle_shape(mDC);
         }
-        
+
+        //먹이 생성
+        //if (Timer1Count % 2 == 0) {
+            for (int i = 1; i < 20; ++i) {
+                shapes[i].cellSizeX = cellSizeX;
+                shapes[i].cellSizeY = cellSizeY;
+                shapes[i].print_food_shape(mDC);
+
+                switch (shapes[i].move_i)
+                {
+                case 0: // 먹이 이동 방법 1
+
+                    if (shapes[i].positionX == 39)
+                        shapes[i].m1_status = true;
+                    if (shapes[i].positionX == 0)
+                        shapes[i].m1_status = false;
+
+                    shapes[i].positionX += (shapes[i].m1_status == true ? -1 : 1); // 방향에 따라 오른쪽 또는 왼쪽으로 이동
+                    break;
+                case 1: // 먹이 이동 방법 2
+                    break;
+                case 2: // 먹이 이동 방법 3
+                    break;
+                case 3: // 먹이 이동 방법 4
+                    break;
+                default:
+                    break;
+                }
+
+            }
+       // }
+       
         //--- 마지막에 메모리 DC의 내용을 화면 DC로 복사한다.
         BitBlt(hDC, 0, 0, rect.right, rect.bottom, mDC, 0, 0, SRCCOPY);
         DeleteDC(mDC);
@@ -216,6 +274,9 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
         switch (wParam) {
         case 1:
             Timer1Count++;
+            break;
+        case 2:
+            Timer2Count++;
             break;
         }
         InvalidateRect(hWnd, NULL, false);
