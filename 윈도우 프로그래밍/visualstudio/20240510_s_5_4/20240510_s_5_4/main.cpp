4f99c2de0b80586ec19d74c02cd0a6c58dfa9779
unknown <kevin018111@gmail.com>
2024-05-11 17:47:10 +0900

    1

--- 윈도우 프로그래밍/visualstudio/20240510_s_5_4/20240510_s_5_4/main.cpp
+++ 윈도우 프로그래밍/visualstudio/20240510_s_5_4/20240510_s_5_4/main.cpp
@@ -16,8 +16,8 @@ ULONG_PTR gdiplusToken;
 
 HINSTANCE g_hInst;
 
-LPCTSTR lpszClass = L"실습 5-3";
-LPCTSTR lpszWindowName = L"실습 5-3";
+LPCTSTR lpszClass = L"실습 5-4";
+LPCTSTR lpszWindowName = L"실습 5-4";
 
 #define WIDTH 1000
 #define HEIGHT 900
@@ -85,9 +85,7 @@ struct Character {
 
     void print_v(HDC& mDC1) {
         if (move_status != -1)
-            //imgs[move_status].Draw(mDC1, x, y, width, height, IMG_CELL_SIZE * (select_img - 1), IMG_CELL_SIZE * (select_img - 1), IMG_CELL_SIZE * select_img, IMG_CELL_SIZE * select_img);
             imgs[move_status].Draw(mDC1, x, y, width, height, IMG_CELL_SIZE * select_img, 0, IMG_CELL_SIZE, IMG_CELL_SIZE);
-
     }
 };
 
@@ -105,31 +103,37 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     static HBITMAP hBit1, hBit2;
     static RECT rect;
     static SIZE size;
-    static Character character;
+    static Character character[6];
+    static bool e_status = true;
+    static bool s_status = true;
+    static CImage backgroundImage;
+    static int selectCharacter = 0;
 
     switch (uMsg)
     {
     case WM_CREATE:
     {
         SetTimer(hWnd, 1, 160, NULL);
-        character.imgs[0].Load(L"among us move right1.png"); // 우로 걷기
-        character.imgs[1].Load(L"among us move left1.png"); // 좌로 걷기
-        character.imgs[2].Load(L"among us move back1.png"); // 위로 걷기
-        character.imgs[3].Load(L"among us move front1.png"); // 아래로 걷기
-        character.imgs[4].Load(L"among us jump right1.png"); // 우 점프
-        character.imgs[5].Load(L"among us jump left1.png"); // 좌 점프
-        character.imgs[6].Load(L"among us move right up1.png"); // 우 상단
-        character.imgs[7].Load(L"among us move right down1.png"); // 우 하단
-        character.imgs[8].Load(L"among us move left up1.png"); // 좌 상단
-        character.imgs[9].Load(L"among us move left down1.png"); // 좌 하단
+        character[0].imgs[0].Load(L"among us move right1.png"); // 우로 걷기
+        character[0].imgs[1].Load(L"among us move left1.png"); // 좌로 걷기
+        character[0].imgs[2].Load(L"among us move back1.png"); // 위로 걷기
+        character[0].imgs[3].Load(L"among us move front1.png"); // 아래로 걷기
+        character[0].imgs[4].Load(L"among us jump right1.png"); // 우 점프
+        character[0].imgs[5].Load(L"among us jump left1.png"); // 좌 점프
+        character[0].imgs[6].Load(L"among us move right up1.png"); // 우 상단
+        character[0].imgs[7].Load(L"among us move right down1.png"); // 우 하단
+        character[0].imgs[8].Load(L"among us move left up1.png"); // 좌 상단
+        character[0].imgs[9].Load(L"among us move left down1.png"); // 좌 하단
 
         for (int i = 0; i < 10; ++i) {
-            if (character.imgs[i].IsNull()) {
+            if (character[0].imgs[i].IsNull()) {
                 wchar_t errorMsg[256];
                 swprintf_s(errorMsg, L"이미지 로드에 실패했습니다: %d번째 이미지", i);
                 MessageBox(hWnd, errorMsg, L"이미지 로드 실패", MB_OK | MB_ICONERROR);
             }
         }
+
+        backgroundImage.Load(L"b.png");
 
         break;
     }
@@ -140,20 +144,20 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
         switch (wParam)
         {
         case VK_LEFT:
-            character.move.left = false;
-            character.select_img = 0;
+            character[selectCharacter].move.left = false;
+            character[selectCharacter].select_img = 0;
             break;
         case VK_RIGHT:
-            character.move.right = false;
-            character.select_img = 0;
+            character[selectCharacter].move.right = false;
+            character[selectCharacter].select_img = 0;
             break;
         case VK_UP:
-            character.move.up = false;
-            character.select_img = 0;
+            character[selectCharacter].move.up = false;
+            character[selectCharacter].select_img = 0;
             break;
         case VK_DOWN:
-            character.move.down = false;
-            character.select_img = 0;
+            character[selectCharacter].move.down = false;
+            character[selectCharacter].select_img = 0;
             break;
         default:
             break;
@@ -165,39 +169,39 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
         switch (wParam)
         {
         case VK_LEFT:
-            character.move.left = true;
-            character.move_status = 1;
+            character[selectCharacter].move.left = true;
+            character[selectCharacter].move_status = 1;
             break;
         case VK_RIGHT:
-            character.move.right = true;
-            character.move_status = 0;
+            character[selectCharacter].move.right = true;
+            character[selectCharacter].move_status = 0;
             break;
         case VK_UP:
-            character.move.up = true;
-            character.move_status = 2;
+            character[selectCharacter].move.up = true;
+            character[selectCharacter].move_status = 2;
             break;
         case VK_DOWN:
-            character.move.down = true;
-            character.move_status = 3;
+            character[selectCharacter].move.down = true;
+            character[selectCharacter].move_status = 3;
             break;
         default:
             break;
         }
 
-        if (character.move.right && character.move.down) { //우 하단
-            character.move_status = 7;
+        if (character[selectCharacter].move.right && character[selectCharacter].move.down) { //우 하단
+            character[selectCharacter].move_status = 7;
         }
 
-        if (character.move.right && character.move.up) { // 우 상단
-            character.move_status = 6;
+        if (character[selectCharacter].move.right && character[selectCharacter].move.up) { // 우 상단
+            character[selectCharacter].move_status = 6;
         }
 
-        if (character.move.left && character.move.down) { // 좌 하단
-            character.move_status = 9;
+        if (character[selectCharacter].move.left && character[selectCharacter].move.down) { // 좌 하단
+            character[selectCharacter].move_status = 9;
         }
         
-        if (character.move.left && character.move.up) { // 좌 상단
-            character.move_status = 8;
+        if (character[selectCharacter].move.left && character[selectCharacter].move.up) { // 좌 상단
+            character[selectCharacter].move_status = 8;
         }
 
         InvalidateRect(hWnd, NULL, false);
@@ -205,14 +209,20 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     case WM_CHAR:
         switch (wParam) 
         {
+        case 'e':
+            SetTimer(hWnd, 2, 160, NULL);
+            break;
+        case 's':
+            SetTimer(hWnd, 3, 160, NULL);
+            break;
         case 'j':
-            character.move.jump_status = true;
-            character.move.jump = 0;
-            if (character.move_status == 1) {
-                character.move_status = 5;
+            character[selectCharacter].move.jump_status = true;
+            character[selectCharacter].move.jump = 0;
+            if (character[selectCharacter].move_status == 1) {
+                character[selectCharacter].move_status = 5;
             }
-            else if (character.move_status == 0) {
-                character.move_status = 4;
+            else if (character[selectCharacter].move_status == 0) {
+                character[selectCharacter].move_status = 4;
             }
             break;
         default:
@@ -239,7 +249,9 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 
         FillRect(mDC1, &rect, (HBRUSH)GetStockObject(WHITE_BRUSH));
 
-        character.print_v(mDC1);
+        backgroundImage.Draw(mDC1, 0, 0, rect.right, rect.bottom);
+
+        character[selectCharacter].print_v(mDC1);
 
         BitBlt(hDC, 0, 0, rect.right, rect.bottom, mDC1, 0, 0, SRCCOPY);
 
@@ -253,52 +265,93 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     case WM_TIMER:
         switch (wParam)
         {
+
+        case 2:
+
+            if (e_status) {
+                character[selectCharacter].width += 10;
+                character[selectCharacter].height +=10;
+                if (character[selectCharacter].width >= 200 && character[selectCharacter].height >= 200) {
+                    e_status = false;
+                }
+            }
+            else {
+                character[selectCharacter].width -= 10;
+                character[selectCharacter].height -= 10;
+                if (character[selectCharacter].width <= 100 && character[selectCharacter].height <= 100) {
+                    e_status = true;
+                    character[selectCharacter].width = 100;
+                    character[selectCharacter].height = 100;
+                    KillTimer(hWnd, 2);
+                }
+            }
+
+            break;
+        case 3:
+            if (e_status) {
+                character[selectCharacter].width -= 10;
+                character[selectCharacter].height -= 10;
+                if (character[selectCharacter].width <= 50 && character[selectCharacter].height <= 50) {
+                    e_status = false;
+                }
+            }
+            else {
+                character[selectCharacter].width += 10;
+                character[selectCharacter].height += 10;
+                if (character[selectCharacter].width >= 100 && character[selectCharacter].height >= 100) {
+                    e_status = true;
+                    character[selectCharacter].width = 100;
+                    character[selectCharacter].height = 100;
+                    KillTimer(hWnd, 3);
+                }
+            }
+            break;
         case 1:
         {
-            Move& move = character.move;
+            Move& move = character[selectCharacter].move;
             if (move.down || move.left || move.up || move.right) {
-                character.select_img++;
-                if (character.select_img == 4) {
-                    character.select_img = 0;
+                character[selectCharacter].select_img++;
+                if (character[selectCharacter].select_img == 4) {
+                    character[selectCharacter].select_img = 0;
                 }
 
                 if (move.down) {
-                    character.y += 7;
+                    character[selectCharacter].y += 7;
                 }
 
                 if (move.left) {
-                    character.x -= 7;
+                    character[selectCharacter].x -= 7;
                 }
 
                 if (move.up) {
-                    character.y -= 7;
+                    character[selectCharacter].y -= 7;
                 }
 
                 if (move.right) {
-                    character.x += 7;
+                    character[selectCharacter].x += 7;
                 }
 
             }
             if (move.jump != -1 && move.jump_status) {
                 move.jump += 10;
-                character.y -= move.jump;
+                character[selectCharacter].y -= move.jump;
                 if (move.jump >= 20) {
                     move.jump_status = false;
                 }
             }
             else if (move.jump != -1 && !move.jump_status) {
                 move.jump += 10;
-                character.y += move.jump;
+                character[selectCharacter].y += move.jump;
                 if (move.jump >= 30) {
                     move.jump = -1;
                     move.jump_status = false;
 
-                    if (character.move_status == 5) {
-                        character.move_status = 1;
+                    if (character[selectCharacter].move_status == 5) {
+                        character[selectCharacter].move_status = 1;
                         move.left = true;
                     }
-                    else if (character.move_status == 4) {
-                        character.move_status = 0;
+                    else if (character[selectCharacter].move_status == 4) {
+                        character[selectCharacter].move_status = 0;
                         move.right = true;
                     }
 
